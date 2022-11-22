21aea42c192744ad845bc1aec0c1abddf044f5a5
CameronDickie <cameronbdickie@gmail.com>
2022-11-22 14:11:57 -0500

    Adding in a hand
    Changed the code so that the player now chooses to join, and then they
    do they are given a hand of two cards

--- api/core.py
+++ api/core.py
@@ -1,7 +1,24 @@
 import time
 import random
 from flask import Flask
+from flask import request, session
+from flask_session import Session
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+class Player:
+    def __init__(self, n):
+        self.name = n
+        self.hand = []
+        self.chips = 0
+        self.handSize = 2
+    def addToHand(self, card):
+        self.hand.append(card)
+    def removeFromHand(self, card):
+        for c in self.hand:
+            if(c.suit == card.suit and c.value == card.value):
+                self.hand.remove(c)
+                return
 
 class Card:
     def __init__(self, val, s):
@@ -41,15 +58,66 @@ class Card:
                 s += "{\"value\":\"" + str(cards[i].value) + "\", \"suit\":\"" + str(cards[i].suit) + "\"},"
         s += "]"
         return s
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+    def drawHand(cards, player):
+        #remove player.handsize cards from cards, and add them to player's hand    
+        for i in range(0, player.handSize):
+            card = cards[0]
+            player.addToHand(card)
+            cards.remove(card)
+
+
+
+
+deck = []
+players = []
 
 @app.route('/init_cards')
 def init_cards():
-    cards = Card.initializeCards()
-    cards = Card.shuffleCards(cards)
+    global deck
+    if(deck == None or deck == []):
+        deck = Card.initializeCards()
+        deck = Card.shuffleCards(deck)
+    else:
+        return {"error": "Deck is already made"}
+    return {"success": "Deck was created"}
 
-    return {'cards': Card.listToString(cards)}
+@app.route('/draw/<user_name>', methods=['POST'])
+def draw_hand(user_name):
+    #draw a hand for the user with user_name
+    global deck, players
+    player = None
+    for p in players:
+        if(p.name == user_name):
+            player = p
+    if(player == None):
+        return {"error": "Player not found"}
+    if(deck == None or deck == []):
+        return {"error": "Deck not initialized"}
+    Card.drawHand(deck, player)
+    for i in range(0, len(players)):
+        if(players[i].name == user_name):
+            players[i] = player
+    return {"success": Card.listToString(player.hand)}
+    
+    
+@app.route('/leave_game/<user_name>', methods=['POST'])
+def leave_game(user_name):
+    #remove player from list of players
+    global deck, players
+    for p in players:
+        if(p.name == user_name):
+            print('removing player')
+            players.remove(p)
+            #readd the players hand to the deck
+            break;
+    return {"success": "The player was removed"}
+
+@app.route('/join_game/<user_name>', methods=['GET', 'POST'])
+def join_game(user_name):
+    global players
+    if(players == None or players == []):
+        p = Player(user_name)
+        players.append(p)
+    return {"success": "The player was added"}
 
 
--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import logo from "./logo.svg";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Card from "./components/Card";
 import axios from "axios";
 import "./App.css";
 
 function App() {
   const [curCards, setCurCards] = useState([{ value: "0", suit: "test" }]);
+  const [inGame, setInGame] = useState(false);
   useEffect(() => {
-    axios.get("/init_cards").then((res) => {
-      let cardList = JSON.parse(res.data["cards"]);
-      setCurCards(cardList);
-    });
-  }, []);
+    if (!inGame) {
+      axios.post("/leave_game/CDickie").then((res) => {
+        console.log(res.data["success"]);
+      });
+    } else {
+      axios.get("/init_cards").then((res) => {
+        if (res.data["error"]) {
+          console.log("cards already initialized");
+        }
+        axios.post("/join_game/CDickie").then((res) => {
+          if (!res.data["success"]) {
+            console.log("error joining game");
+            return "";
+          }
+          axios.post("/draw/CDickie").then((res) => {
+            if (!res.data["success"]) {
+              console.log("error drawing hand");
+              return "";
+            }
+            let cardList = JSON.parse(res.data["success"]);
+            setCurCards(cardList);
+          });
+        });
+      });
+    }
+  }, [inGame]);
   return (
     <div>
-      {(curCards || []).map((item) => (
-        <Card key={JSON.stringify(item)} card={item} />
-      ))}
+      <div>
+        {((inGame && curCards) || []).map((item) => (
+          <Card key={JSON.stringify(item)} card={item} />
+        ))}
+      </div>
+      <button
+        onClick={() => {
+          setInGame(!inGame);
+        }}
+      >
+        Join/Leave Game
+      </button>
     </div>
   );
 }
