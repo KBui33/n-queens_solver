9c729cf0e831f12d9bed91732e7bc2bd37e3127f
KBui33 <kevinbui3@cmail.carleton.ca>
2022-12-08 01:35:46 -0500

    Can create a grid by var n

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [size, setSize] = useState(0);
+  const [board, setBoard] = useState([[]]);
+
+  const gridbox = {
+    width: 50 * size,
+  };
+
+  useEffect(() => {
+    setSize(5);
+  }, []);
+
+  useEffect(() => {
+    console.log(`chaning board size to ${size}`);
+
+    let board = [];
+    for (var i = 1; i <= size; i++) {
+      let temp = [];
+      for (var j = 1; j <= size; j++) {
+        temp.push(0);
+      }
+      board.push(temp);
+    }
+    setBoard(board);
+  }, [size]);
+
+  const test = (e) => {
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === "" || re.test(e.target.value)) {
+      setSize(e.target.value);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {board.map((items, idx) => {
+          return (
+            <div className="row">
+              {items.map((subItem, subIdx) => {
+                return <div className="tile"></div>;
+              })}
+            </div>
+          );
+        })}
+      </div>
+      <div className="options">
+        <input value={size} onChange={test}></input>
+        <button className="start-btn">Get Combos</button>
+      </div>
     </div>
   );
 }
